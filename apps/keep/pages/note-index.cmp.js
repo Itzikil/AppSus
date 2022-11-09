3c64878fe80d80047c02d020fa25237e2c27e5c6
itzhake <itzikil@icloud.com>
2022-11-09 12:11:57 +0200

    1.32

--- apps/keep/pages/note-index.cmp.js
+++ apps/keep/pages/note-index.cmp.js
@@ -2,7 +2,16 @@
 export default {
     template:`
         <section>
-            <h1>hi</h1>
+            <h1>note</h1>
+            <font-awesome-icon icon="fa-brands fa-youtube" />
+            <p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
+            <p>
+                <i class="fa fa-youtube-play" aria-hidden="true"></i>
+            </p>
+            <p>
+            <i class="fa fa-comment-o" aria-hidden="true"></i>
+        </p>
+        <p><i class="fa fa-list-ul" aria-hidden="true"></i></p>
         </section>
     `
 }
