6e3d6cd31dd21d864073c46642fd8142f6a203a2
Shahar Haviv <shaharhaviv8@gmail.com>
2022-11-09 18:03:11 +0200

    tr td --> ul li spans

--- apps/mail/cmps/email-list.cmp.js
+++ apps/mail/cmps/email-list.cmp.js
@@ -2,17 +2,16 @@ import emailPreview from "./email-preview.cmp.js"
 export default {
     props: ['emails'],
     template: `
-    <tbody >
-        <tr>
-            <td>From</td>
-            <td>Subject</td>
-            <td>Body</td>
-            <td>Sent At</td>
-        </tr>
-        <tr v-for="email in emails" :key="email.id">
-            <email-preview :email="email"/>
-        </tr>
-    </tbody>
+   
+    <section class="email-list">
+        <ul class="clean-list">
+            <li v-for="email in emails" :key="email.id">
+                <email-preview :email="email"/>
+            </li>
+        </ul>
+    </section>
+
+
     `,
     components: {
         emailPreview,
