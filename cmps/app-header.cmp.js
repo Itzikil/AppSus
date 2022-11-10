c664f32361500f6deb3d6ba3ae84510b76e15d56
itzhake <itzikil@icloud.com>
2022-11-10 20:42:02 +0200

    2.5

--- cmps/app-header.cmp.js
+++ cmps/app-header.cmp.js
@@ -4,9 +4,9 @@ export default {
             <h1>AppSus</h1>
             <nav>
                 <router-link to="/">Home</router-link> | 
-                <router-link to="/about">About</router-link> |
                 <router-link to="/note">Notes</router-link> |
-                <router-link to="/email/inbox">Emails</router-link>
+                <router-link to="/email/inbox">Emails</router-link> |
+                <router-link to="/about">About</router-link> 
             </nav>
         </header>
     `,
