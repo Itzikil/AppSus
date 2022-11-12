35e25e4a99cb6d4f51e811f229ec088e340b4da3
Shahar Haviv <shaharhaviv8@gmail.com>
2022-11-12 16:25:57 +0200

    email styling +filter faeture+ mail status +sort

--- apps/mail/services/email.service.js
+++ apps/mail/services/email.service.js
@@ -11,9 +11,10 @@ export const emailService = {
     remove,
     save,
     get,
-    queryUser,
+    queryUser, 
+    showTime,
 }
-function queryUser(){
+function queryUser() {
     return storageService.query(USER_KEY)
 }
 function query() {
@@ -35,42 +36,253 @@ function _createEmails() {
     let emails = storageService.query(EMAIL_KEY)
     if (!emails || !emails.length) {
         const email = {
+            body: '../keep-images/nature.jpg',
+            from: 'Itzhak@Shahar.thebeks',
             id: utilService.makeId(),
-            subject: 'Miss you!',
-            body: 'Would love to catch up sometimes',
-            sentAt: new Date(),
-            to: 'momo@momo.com',
-            isStarred: false,
-            isRead: true,
-        }
-        const email2 = {
-            id: utilService.makeId(),
-            subject: 'Love you!',
-            body: 'Would love to hang up with you',
-            sentAt: new Date(),
-            to: 'baba@momo.com',
-            isStarred: true,
             isRead: false,
+            isStarred: false,
+            sentAt: showTime(new Date),
+            status: 'sent',
+            subject: 'Bobi and Me',
+            to: 'Itzhak@Shahar.thebeks',
+        }
+        const email5 = {
+            body: '../keep-images/nature.jpg',
+            from: 'Itzhak@Shahar.thebeks',
+            id: utilService.makeId(),
+            isRead: false,
+            isStarred: false,
+            sentAt: showTime(new Date),
+            status: 'sent',
+            subject: 'Bobi and Me',
+            to: 'Itzhak@Shahar.thebeks',
         }
         const email3 = {
+            body: '../keep-images/nature.jpg',
+            from: 'Itzhak@Shahar.thebeks',
             id: utilService.makeId(),
-            subject: 'Hate you!',
-            body: 'Never want to see you again!',
-            sentAt: new Date(),
-            to: 'dada1@momo.com',
-            isStarred: false,
             isRead: false,
+            isStarred: false,
+            sentAt: showTime(new Date),
+            status: 'sent',
+            subject: 'Bobi and Me',
+            to: 'Itzhak@Shahar.thebeks',
         }
-        emails = [email, email2, email3]
+        const email9 = {
+            body: '../keep-images/nature.jpg',
+            from: 'Itzhak@Shahar.thebeks',
+            id: utilService.makeId(),
+            isRead: false,
+            isStarred: false,
+            sentAt: showTime(new Date),
+            status: 'sent',
+            subject: 'Bobi and Me',
+            to: 'Itzhak@Shahar.thebeks',
+        }
+        const email2 = {
+            body: '../keep-images/nature.jpg',
+            from: 'Itzhak@Shahar.thebeks',
+            id: utilService.makeId(),
+            isRead: false,
+            isStarred: false,
+            sentAt: showTime(new Date),
+            status: 'sent',
+            subject: 'Bobi and Me',
+            to: 'Itzhak@Shahar.thebeks',
+        }
+        const email6 = {
+            body: '../keep-images/nature.jpg',
+            from: 'Itzhak@Shahar.thebeks',
+            id: utilService.makeId(),
+            isRead: false,
+            isStarred: false,
+            sentAt: showTime(new Date),
+            status: 'sent',
+            subject: 'Bobi and Me',
+            to: 'Itzhak@Shahar.thebeks',
+        }
+        const email7 = {
+            body: 'If you need any help im here for not including weekend and i work only from 8am-12pm have a nice one',
+            from: 'Itzhak@Shahar.thebeks',
+            id: utilService.makeId(),
+            isRead: true,
+            isStarred: false,
+            sentAt: showTime(new Date),
+            status: 'inbox',
+            isTrash: false,
+            subject: 'Work on design',
+            to: 'Itzhak@Shahar.thebeks',
+        }
+        const email8 = {
+            body: 'If you need any help im here for not including weekend and i work only from 8am-12pm have a nice one',
+            from: 'Itzhak@Shahar.thebeks',
+            id: utilService.makeId(),
+            isRead: true,
+            isStarred: false,
+            sentAt: showTime(new Date),
+            status: 'inbox',
+            isTrash: false,
+            subject: 'Work on design',
+            to: 'Itzhak@Shahar.thebeks',
+        }
+        const email4 = {
+            body: 'If you need any help im here for not including weekend and i work only from 8am-12pm have a nice one',
+            from: 'Itzhak@Shahar.thebeks',
+            id: utilService.makeId(),
+            isRead: true,
+            isStarred: false,
+            sentAt: showTime(new Date),
+            status: 'inbox',
+            isTrash: false,
+            subject: 'Work on design',
+            to: 'Itzhak@Shahar.thebeks',
+        }
+        const email10 = {
+            body: 'If you need any help im here for not including weekend and i work only from 8am-12pm have a nice one',
+            from: 'Itzhak@Shahar.thebeks',
+            id: utilService.makeId(),
+            isRead: true,
+            isStarred: false,
+            sentAt: showTime(new Date),
+            status: 'inbox',
+            isTrash: false,
+            subject: 'Work on design',
+            to: 'Itzhak@Shahar.thebeks',
+        }
+        const email16 = {
+            body: 'If you need any help im here for not including weekend and i work only from 8am-12pm have a nice one',
+            from: 'Itzhak@Shahar.thebeks',
+            id: utilService.makeId(),
+            isRead: true,
+            isStarred: false,
+            sentAt: showTime(new Date),
+            status: 'inbox',
+            isTrash: true,
+            subject: 'Work on design',
+            to: 'Itzhak@Shahar.thebeks',
+        }
+        const email15 = {
+            body: 'If you need any help im here for not including weekend and i work only from 8am-12pm have a nice one',
+            from: 'Itzhak@Shahar.thebeks',
+            id: utilService.makeId(),
+            isRead: true,
+            isStarred: false,
+            sentAt: showTime(new Date),
+            status: 'inbox',
+            isTrash: true,
+            subject: 'Work on design',
+            to: 'Itzhak@Shahar.thebeks',
+        }
+        const email13 = {
+            body: 'If you need any help im here for not including weekend and i work only from 8am-12pm have a nice one',
+            from: 'Itzhak@Shahar.thebeks',
+            id: utilService.makeId(),
+            isRead: true,
+            isStarred: false,
+            sentAt: showTime(new Date),
+            status: 'inbox',
+            isTrash: true,
+            subject: 'Work on design',
+            to: 'Itzhak@Shahar.thebeks',
+        }
+        const email14 = {
+            body: 'If you need any help im here for not including weekend and i work only from 8am-12pm have a nice one',
+            from: 'Itzhak@Shahar.thebeks',
+            id: utilService.makeId(),
+            isRead: true,
+            isStarred: false,
+            sentAt: showTime(new Date),
+            status: 'inbox',
+            isTrash: true,
+            subject: 'Work on design',
+            to: 'Itzhak@Shahar.thebeks',
+        }
+        const email12 = {
+            body: 'Would you tell us how to make a canvas display on a note?',
+            from: 'Itzhak@Shahar.thebeks',
+            id: utilService.makeId(),
+            isRead: false,
+            isStarred: true,
+            sentAt: showTime(new Date),
+            status: 'inbox',
+            subject: 'Any suggestions?',
+            to: 'Itzhak@Shahar.thebeks',
+        }
+        const email11 = {
+            body: 'Would you tell us how to make a canvas display on a note?',
+            from: 'Itzhak@Shahar.thebeks',
+            id: utilService.makeId(),
+            isRead: false,
+            isStarred: true,
+            sentAt: showTime(new Date),
+            status: 'inbox',
+            subject: 'Any suggestions?',
+            to: 'Itzhak@Shahar.thebeks',
+        }
+        const email17 = {
+            body: 'Would you tell us how to make a canvas display on a note?',
+            from: 'Itzhak@Shahar.thebeks',
+            id: utilService.makeId(),
+            isRead: false,
+            isStarred: true,
+            sentAt: showTime(new Date),
+            status: 'inbox',
+            subject: 'Any suggestions?',
+            to: 'Itzhak@Shahar.thebeks',
+        }
+        const email18 = {
+            body: 'Would you tell us how to make a canvas display on a note?',
+            from: 'Itzhak@Shahar.thebeks',
+            id: utilService.makeId(),
+            isRead: false,
+            isStarred: true,
+            sentAt: showTime(new Date),
+            status: 'inbox',
+            subject: 'Any suggestions?',
+            to: 'Itzhak@Shahar.thebeks',
+        }
+        emails = [
+            email,
+            email2,
+            email3,
+            email4,
+            email5,
+            email6,
+            email7,
+            email8,
+            email9,
+            email10,
+            email11,
+            email12,
+            email13,
+            email14,
+            email15,
+            email16,
+            email17,
+            email18,
+        ]
         // console.log(emails)
         utilService.save(EMAIL_KEY, emails)
     }
     return emails
 }
+function showTime(time) {
+    var today = time
+    var dd = String(today.getDate()).padStart(2, '0')
+    var mm = String(today.getMonth() + 1).padStart(2, '0')
+    var yyyy = today.getFullYear()
+
+    today = dd + '/' + mm + '/' + yyyy
+    return today
+}
 function _createUser() {
-    const loggedinUser = utilService.load(USER_KEY) || {
-        email: 'user@appsus.com',
-        fullname: 'Mahatma Appsus',
+    let loggedinUser = utilService.load(USER_KEY)
+    if (!loggedinUser) {
+        loggedinUser = {
+            email: 'Itzhak@Shahar.thebeks',
+            fullname: 'Call Me Appsus',
+        }
+        utilService.save(USER_KEY, loggedinUser)
     }
     return loggedinUser
 }
---
+++ views/email-details.cmp.js
@@ -0,0 +1,50 @@
+import { emailService } from "../apps/mail/services/email.service.js";
+export default {
+  template: `
+    <section v-if="email" class="flex flex-column email-details email-app">
+      <div class="email-user flex flex-column">
+        <div class="email-user-container flex align-center">
+          <router-link :to="emailListUrl"><i class="fa fa-arrow-left"></i></router-link>
+          <i class="fa fa-user-circle flex align-center justify-center"></i>
+          <h3>{{email.from}}</h3>
+          <p>{{showEmailSentAtTime}}</p>
+        </div>
+        <small class="text-secondary">to {{email.to}}</small>
+      </div>
+      <div class="email-content">
+        <p>{{email.body}}</p>
+      </div>
+    </section>
+  `,
+  data() {
+    return {
+      email: null,
+    };
+  },
+  created() {
+    const id = this.$route.params.id
+    emailService.get(id).then(email => {
+      this.email = email
+      this.email.isRead = true
+      emailService.save(this.email)
+    })
+  },
+  methods: {
+    removeEmail() {
+      // TODO: use event bus to emit the event to email-app
+    },
+  },
+  computed: {
+    emailListUrl() {
+      const status = this.$route.params.status;
+      return '/email/' + status;
+    },
+    showEmailSentAtTime() {
+      return new Date(this.email.sentAt)
+        .toString()
+        .slice(0, 24)
+        .replaceAll(' ', ',');
+    },
+  },
+  unmounted() { },
+};
